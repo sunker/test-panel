7a9c15ae49cd32a173e3b3da4f762e2064786766
Erik Sundell <erik.sundell87@gmail.com>
2025-01-30 15:30:54 +0100

    t

--- .github/actions/playwright-gh-pages/deploy-report-pages/build-pr-comment.js
+++ .github/actions/playwright-gh-pages/deploy-report-pages/build-pr-comment.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+async function checkUrlExists(url) {
+  try {
+    const response = await fetch(url);
+    return response.status !== 404;
+  } catch (error) {
+    console.error('Error fetching URL:', error);
+    return false; // Assume false if there's an error (e.g., network issue)
+  }
+}
+
 // Ensure we are in the right directory
 const reportsDir = 'all-reports';
 if (!fs.existsSync(reportsDir) || !fs.statSync(reportsDir).isDirectory()) {
@@ -32,6 +42,8 @@ if (usePluginName) {
 
 // Initialize an array to store rows
 let rows = [];
+let uploadReportDisabled = false;
+let lastReportLink = '';
 
 // Iterate through subdirectories
 fs.readdirSync(reportsDir).forEach((dir) => {
@@ -52,12 +64,14 @@ fs.readdirSync(reportsDir).forEach((dir) => {
   const grafanaVersion = getValue('GRAFANA_VERSION');
   const testOutput = getValue('OUTPUT');
   const pluginName = getValue('PLUGIN_NAME');
+  uploadReportDisabled = getValue('UPLOAD_REPORT_ENABLED') === 'false';
 
   // Construct report link
   const repoOwner = process.env.GITHUB_REPOSITORY_OWNER;
   const repoName = process.env.GITHUB_REPOSITORY_NAME;
   const timestamp = process.env.TIMESTAMP;
   const jobInitiator = process.env.JOB_INITIATOR;
+  const branchName = process.env.BRANCH;
 
   const reportLink = pluginName
     ? `https://${repoOwner}.github.io/${repoName}/${timestamp}/${jobInitiator}/${pluginName}-${grafanaImage}-${grafanaVersion}/`
@@ -69,6 +83,10 @@ fs.readdirSync(reportsDir).forEach((dir) => {
   // Check for index.html
   const hasReport = fs.existsSync(path.join(dirPath, 'index.html'));
   const reportCell = hasReport ? `[View report](${reportLink})` : ' ';
+
+  if (hasReport) {
+    lastReportLink = reportLink;
+  }
 
   // Add row to table
   if (usePluginName) {
@@ -86,5 +104,15 @@ rows.sort((a, b) => {
 
 // Add sorted rows to table
 table += '\n' + rows.join('\n');
+
+if (uploadReportDisabled) {
+  table +=
+    '\n > **_NOTE:_**  To be able to browse the Playwright reports for failing end-to-end tests, enable the `upload-report` input in the `upload-report-artifacts` Action.';
+}
+
+const urlExist = await checkUrlExists(lastReportLink);
+if (!urlExist) {
+  table += `\n > **_WARNING:_**  The reports were deployed to GitHub Pages, but it seems like GitHub Pages is not configured to deploy from ${branchName}. For details, refer to the [README](https://github.com/grafana/plugin-actions/main/deploy-report-pages/README.md) for the deploy-report-pages Action.`;
+}
 
 console.log(table);
